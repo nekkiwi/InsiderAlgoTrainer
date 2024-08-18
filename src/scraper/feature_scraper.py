b233130e347a70e5550bf8926aedb3bb96ba8ece
Nekki97 <nektarios.winter@yahoo.de>
2024-08-18 12:19:09 +0200

    Implement save data and aggregate multiple entries

--- src/scraper/feature_scraper.py
+++ src/scraper/feature_scraper.py
@@ -4,13 +4,13 @@ import pandas as pd
 import sys
 import os
 from io import StringIO
+import openpyxl
 import yfinance as yf
 
 # Add the 'src' directory to the system path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from utils.date_helpers import get_next_market_open
-from utils.scraper_helpers import parse_titles, clean_numeric_columns, calculate_technical_indicators
+from utils.scraper_helpers import parse_titles, clean_numeric_columns, calculate_technical_indicators, process_dates, aggregate_group
 from utils.stock_helpers import download_stock_data
 
 class FeatureScraper:
@@ -29,45 +29,51 @@ class FeatureScraper:
         self.data = pd.read_html(StringIO(str(table)))[0]
         # Clean up column names by replacing \xa0 with a regular space
         self.data.columns = self.data.columns.str.replace('\xa0', ' ', regex=False)
-        # List of columns to extract
-        columns_of_interest = ["Filing Date", "Trade Date", "Ticker", "Title", "Price", "Qty", "Owned", "ΔOwn", "Value"]
-        # Filter the DataFrame to include only these columns
-        self.data = self.data[columns_of_interest]
     
     def clean_table(self):
-        # Convert date strings to datetime objects
-        self.data['Filing Date'] = pd.to_datetime(self.data['Filing Date']).apply(get_next_market_open)
-        self.data['Trade Date'] = pd.to_datetime(self.data['Trade Date'])
-        
-        # Calculate "Days Since Trade"
-        self.data['Days Since Trade'] = (self.data['Filing Date'] - self.data['Trade Date']).dt.days
-        
-        self.data = clean_numeric_columns(self.data)
-        self.data = parse_titles(self.data)
-        
+        columns_of_interest = ["Filing Date", "Trade Date", "Ticker", "Title", "Price", "Qty", "Owned", "ΔOwn", "Value"]
+        self.data = self.data[columns_of_interest]
+        process_dates(self)
+        clean_numeric_columns(self)
+        parse_titles(self)
         self.data.drop(columns=['Title', 'Trade Date'], inplace=True)
+        # Group by Ticker and Filing Date, then aggregate
+        aggregate_group(self)
+        self.data['Filing Date'] = self.data['Filing Date'].dt.strftime('%d-%m-%Y %H:%M')
+        # Drop rows with any NaN in the specified columns
+        self.data.dropna(inplace=True)
         
     def add_technical_indicators(self):
         """Add technical indicators to the DataFrame."""
         def process_ticker(row):
             ticker = row['Ticker']
             stock_data = download_stock_data(ticker)
-            if stock_data is None or stock_data.empty:
-                print(f"Skipping ticker {ticker} due to missing data.")
-                return row  # Return the row unchanged
-            indicators = calculate_technical_indicators(stock_data)
-            for key, value in indicators.items():
-                if not value.empty:
-                    row[key] = value.iloc[-1]  # Get the most recent value of each indicator
+            row = calculate_technical_indicators(row, stock_data)
             return row
 
         # Apply the processing function to each row in the DataFrame
-        self.data = self.data.apply(process_ticker, axis=1)
-        return self.data
+        self.data = self.data.apply(process_ticker, axis=1, inplace=True)
+    
+    def save_to_excel(self, file_path='output.xlsx'):
+        """Save the self.data DataFrame to an Excel file."""
+        data_dir = os.path.join(os.path.dirname(__file__), '../../data')
+        os.makedirs(data_dir, exist_ok=True)  # Create the data directory if it doesn't exist
         
-    def process_features(self):
+        file_path = os.path.join(data_dir, file_path)
+        if not self.data.empty:
+            try:
+                self.data.to_excel(file_path, index=False)
+                print(f"Data successfully saved to {file_path}.")
+            except Exception as e:
+                print(f"Failed to save data to Excel: {e}")
+        else:
+            print("No data to save.")
+        
+    def extract_features(self):
         html = self.get_html(scraper.base_url)
         self.parse_table(html)
+        
+    def process_features(self):
         self.clean_table()
         # add technical indicators
         # self.add_technical_indicators()
@@ -76,5 +82,9 @@ class FeatureScraper:
         
 if __name__ == "__main__":
     scraper = FeatureScraper()
+    
+    scraper.extract_features()
+    scraper.save_to_excel('raw/features.xlsx')
+    
     scraper.process_features()
-    print(scraper.data.head())
+    scraper.save_to_excel('interim/features.xlsx')
--- src/utils/scraper_helpers.py
+++ src/utils/scraper_helpers.py
@@ -1,33 +1,58 @@
 import yfinance as yf
 import talib
+import pandas as pd
+from utils.date_helpers import get_next_market_open
 
-def clean_numeric_columns(df):
-    df['Price'] = df['Price'].replace({r'\$': '', r',': ''}, regex=True).astype(float)
-    df['Qty'] = df['Qty'].replace({r',': ''}, regex=True).astype(int)
-    df['Owned'] = df['Owned'].replace({r',': ''}, regex=True).astype(int)
-    df['Value'] = df['Value'].replace({r'\$': '', r',': '', r'\+': ''}, regex=True).astype(float)
-    df['ΔOwn'] = df['ΔOwn'].replace({
-        r'%': '', r'\+': '', r'New': '999', r'>': ''
-    }, regex=True).astype(float)
-    return df
-
-def parse_titles(df):
-    df['CEO'] = df['Title'].apply(lambda title: int('CEO' in title))
-    df['CFO'] = df['Title'].apply(lambda title: int('CFO' in title))
-    df['COO'] = df['Title'].apply(lambda title: int('COO' in title))
-    df['Dir'] = df['Title'].apply(lambda title: int('Dir' in title))
-    df['Pres'] = df['Title'].apply(lambda title: int('Pres' in title))
-    df['VP'] = df['Title'].apply(lambda title: int('VP' in title))
-    df['10%'] = df['Title'].apply(lambda title: int('10%' in title))
-    return df
-
-def calculate_technical_indicators(stock_data):
-    df = {}
-    # Example of calculating various indicators
-    df['SMA_50'] = talib.SMA(stock_data['Close'], timeperiod=50)
-    df['EMA_50'] = talib.EMA(stock_data['Close'], timeperiod=50)
-    df['RSI_14'] = talib.RSI(stock_data['Close'], timeperiod=14)
-    df['MACD'], df['MACD_Signal'], _ = talib.MACD(stock_data['Close'])
+def process_dates(self):
+    # Convert date strings to datetime objects
+    self.data['Filing Date'] = pd.to_datetime(self.data['Filing Date']).apply(get_next_market_open)
+    self.data['Trade Date'] = pd.to_datetime(self.data['Trade Date'])
     
-    # Add more indicators as needed
-    return df
+    # Calculate "Days Since Trade"
+    self.data['Days Since Trade'] = (self.data['Filing Date'] - self.data['Trade Date']).dt.days
+
+def clean_numeric_columns(self):
+    self.data['Price'] = self.data['Price'].replace({r'\$': '', r',': ''}, regex=True).astype(float)
+    self.data['Qty'] = self.data['Qty'].replace({r',': ''}, regex=True).astype(int)
+    self.data['Owned'] = self.data['Owned'].replace({r',': ''}, regex=True).astype(int)
+    self.data['Value'] = self.data['Value'].replace({r'\$': '', r',': '', r'\+': ''}, regex=True).astype(float)
+    self.data['ΔOwn'] = self.data['ΔOwn'].replace({r'%': '', r'\+': '', r'New': '999', r'>': ''}, regex=True).astype(float)
+
+def parse_titles(self):
+    self.data['CEO'] = self.data['Title'].apply(lambda title: int('CEO' in title))
+    self.data['CFO'] = self.data['Title'].apply(lambda title: int('CFO' in title))
+    self.data['COO'] = self.data['Title'].apply(lambda title: int('COO' in title))
+    self.data['Dir'] = self.data['Title'].apply(lambda title: int('Dir' in title))
+    self.data['Pres'] = self.data['Title'].apply(lambda title: int('Pres' in title))
+    self.data['VP'] = self.data['Title'].apply(lambda title: int('VP' in title))
+    self.data['10%'] = self.data['Title'].apply(lambda title: int('10%' in title))
+
+def calculate_technical_indicators(row, stock_data):
+    indicators = {}
+    # Example of calculating various indicators
+    indicators['SMA_50'] = talib.SMA(stock_data['Close'], timeperiod=50)
+    indicators['EMA_50'] = talib.EMA(stock_data['Close'], timeperiod=50)
+    indicators['RSI_14'] = talib.RSI(stock_data['Close'], timeperiod=14)
+    indicators['MACD'], indicators['MACD_Signal'], _ = talib.MACD(stock_data['Close'])
+    
+    for key, value in indicators.items():
+        row[key] = value.iloc[-1]  # Get the most recent value of each indicator
+        
+    return row
+
+def aggregate_group(self):
+    # Group by Ticker and Filing Date, then aggregate
+    self.data = self.data.groupby(['Ticker', 'Filing Date']).agg(
+        Number_of_Purchases=('Ticker', 'size'),
+        Price=('Price', 'mean'),
+        Qty=('Qty', 'sum'),
+        Owned=('Owned', 'mean'),
+        ΔOwn=('ΔOwn', 'mean'),
+        Value=('Value', 'sum'),
+        CEO=('CEO', 'max'),
+        CFO=('CFO', 'max'),
+        COO=('COO', 'max'),
+        Dir=('Dir', 'max'),
+        Pres=('Pres', 'max'),
+        VP=('VP', 'max'),
+        TenPercent=('10%', 'max')).sort_values(by='Filing Date', ascending=False).reset_index()
