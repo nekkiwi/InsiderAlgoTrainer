17cc541cfdd5c079619a482461facfd1ac84233c
Nektarios <nektarios.winter@github.com>
2024-09-12 01:55:22 +0200

    first (wrong) working version of backtest

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from src.training.feature_selector import FeatureSelector
 from src.data_exploration.feature_analysis import FeatureAnalyzer
 from src.data_exploration.stock_analysis import StockAnalysis
 from src.training.train import ModelTrainer
+from src.backtesting.backtest import Backtester
 
 def main():    
     ################################
@@ -17,6 +18,7 @@ def main():
     stock_analyzer      = StockAnalysis()
     target_scraper      = TargetScraper()
     feature_selector    = FeatureSelector()
+    backtester          = Backtester()
     
     features_df                 = None
     return_df                   = None
@@ -56,10 +58,10 @@ def main():
 
     # selected_features = feature_selector.run(features_df_filtered, targets_df, p_threshold=0.05)
     
-    models  = ['NeuralNetUnderSample', 'NeuralNetOverSample',    'RandomForestUnderSample', 'RandomForestOverSample']
+    models  = ['NeuralNetUnderSample', 'NeuralNetOverSample', 'RandomForestUnderSample', 'RandomForestOverSample']
     targets = ['pos_return_1m_raw',    'pos_alpha_1m_raw',       'final_return_1m_raw',     'final_alpha_1m_raw',
-               'pos_return_1m_limstop','final_return_1m_limstop','pos_alpha_1m_limstop',    'final_alpha_1m_limstop']
-               #'return_limit_sell',    'return_stop_sell',       'alpha_limit_sell',        'alpha_stop_sell']
+               'pos_return_1m_limstop','final_return_1m_limstop','pos_alpha_1m_limstop',    'final_alpha_1m_limstop',
+               'return_limit_sell',    'return_stop_sell',       'alpha_limit_sell',        'alpha_stop_sell']
 
     for model in models: 
         for target in targets:
@@ -69,5 +71,10 @@ def main():
     # Backtesting #
     ###############
     
+    # limit_array = [ 0.02,  0.04,  0.06, 0.08,   0.1]
+    # stop_array  = [-0.14, -0.12, -0.1, -0.08, -0.06, -0.04, -0.02]
+
+    # backtester.run(limit_array, stop_array)
+
 if __name__ == "__main__":
     main()
--- src/backtesting/backtest.py
+++ src/backtesting/backtest.py
@@ -1,12 +1,15 @@
-from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import Pool, cpu_count
 from tqdm import tqdm
+import time
+from datetime import timedelta
+import os
 from .utils.backtest_helpers import load_stock_data, gather_prediction_gt_pairs, process_prediction_pair, save_results
 
 class Backtester:
     def __init__(self):
         data_dir = os.path.join(os.path.dirname(__file__), '../../data')
         self.predictions_dir = os.path.join(data_dir, "training/predictions")
-        self.stock_data_file = os.path.join(data_dir, "final/stock_data.xlsx")  # Stock data with returns and alpha
+        self.stock_data_file = os.path.join(data_dir, "final/stock_data_final.xlsx")  # Stock data with returns and alpha
         self.output_file = os.path.join(data_dir, "backtest/backtesting_results.xlsx")
         self.limit_array = []
         self.stop_array = []
@@ -14,19 +17,25 @@ class Backtester:
 
     def process_predictions_parallel(self, prediction_gt_pairs):
         """Process predictions and GT in parallel using multiprocessing."""
+        # Prepare arguments for multiprocessing
+        args_list = [
+            (df, model_name, target_name, pair, self.limit_array, self.stop_array, self.stock_data)
+            for df, model_name, target_name, pair in prediction_gt_pairs]
+
         all_results = []
-        with ProcessPoolExecutor() as executor:
-            futures = [
-                executor.submit(process_prediction_pair, (df, model_name, target_name, pair, self.limit_array, self.stop_array, self.stock_data))
-                for df, model_name, target_name, pair in prediction_gt_pairs
-            ]
-            for future in tqdm(futures, desc="Processing backtest results"):
-                all_results.extend(future.result())
+        # Using multiprocessing Pool
+        with Pool(cpu_count()) as pool:
+            for result in tqdm(pool.imap_unordered(process_prediction_pair, args_list), total=len(args_list), desc="- Processing backtest results"):
+                all_results.extend(result)
+        
         return all_results
 
     def run(self, limit_array, stop_array):
         """Run the full backtesting process."""
+        start_time = time.time()
+        print(f"\n### START ### Backtesting")
         # 1. Load stock data and prediction pairs
+        print("- Loading stock data")
         self.stock_data = load_stock_data(self.stock_data_file)
         self.limit_array = limit_array
         self.stop_array = stop_array
@@ -37,3 +46,5 @@ class Backtester:
 
         # 3. Save the final results
         save_results(all_results, self.output_file)
+        elapsed_time = timedelta(seconds=int(time.time() - start_time))
+        print(f"### END ### Backtesting - time elapsed: {elapsed_time}")
